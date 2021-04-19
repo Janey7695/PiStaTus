63d07973fcafdb32e73f33195e82542fe74b745e
Janey_raspiberryPi-armhf <1737941060@qq.com>
2021-04-19 13:00:07 +0800

    add cpuUsageRageLineChart

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 PiStaTus:PiStaTus.o oled.o draw_api.o
-	gcc -o PiStaTus PiStaTus.o oled.o draw_api.o -lwiringPi -lm
-
+	@gcc -o PiStaTus PiStaTus.o oled.o draw_api.o -lwiringPi -lm
+	@echo "Build PiStaTus successfully"
 PiStaTus.o:PiStaTus.c
-	gcc -c PiStaTus.c
-
+	@gcc -c PiStaTus.c
+	@echo "Build dep PiStaTus.o successfully"
 oled.o:./Oled_driver/oled.c
-	gcc -c ./Oled_driver/oled.c
-
+	@gcc -c ./Oled_driver/oled.c
+	@echo "Build oled base driver"
 draw_api.o:./Oled_driver/draw_api.c ./Oled_driver/oled.c
-	gcc -c ./Oled_driver/draw_api.c ./Oled_driver/oled.c
-
+	@gcc -c ./Oled_driver/draw_api.c ./Oled_driver/oled.c
+	@echo "Build oled api"
 
 clean:
 	rm -f *.o
--- PiStaTus.c
+++ PiStaTus.c
@@ -15,6 +15,11 @@
 #define UsageRectWidth 5
 #define UsageRectLength 80
 #define UsageRectInter 2
+#define LineChartLength 40
+#define LineChartWidth 27
+#define LineChartBegin_x 85
+#define LineChartBegin_y 35
+
 
 typedef struct cpu_n
 {
@@ -28,13 +33,13 @@ typedef struct cpu_n
 
 typedef struct cpu_a
 {
-    cpu_usage cpu[4];
+    cpu_usage cpu[5];
 }cpu_all;
 
-cpu_usage cpu_1,cpu_2,cpu_3,cpu_4;
+cpu_usage cpu_1,cpu_2,cpu_3,cpu_4,cpu_Al;
 cpu_all CpuUsage;
 int temp = 0;
-
+char cpuAllUsageLineChartDat[LineChartLength];
 char* TempFilePathCmd = "cat /sys/class/thermal/thermal_zone0/temp";
 char* CpuInfoPath = "/proc/stat";
 
@@ -44,12 +49,24 @@ void GetCpuUasge(void);
 
 void Draw_UI()
 {
-    
+    OledPaint.Draw.Rect(LineChartBegin_x,LineChartBegin_y,LineChartBegin_x+LineChartLength,LineChartBegin_y+LineChartWidth,0);
     OledPaint.Draw.Picture(0,0,38,28,cpu_temp_img);
     for(int i=0;i<4;i++)
     {
         OledPaint.Draw.Rect(UsageRectBeigin_x,UsageRectBeigin_y+(UsageRectWidth+UsageRectInter)*i,UsageRectBeigin_x+UsageRectLength,UsageRectWidth+UsageRectBeigin_y+(UsageRectWidth+UsageRectInter)*i,0);
     }
+}
+
+void updateLineCHartDat(cpu_all* CpuUsAge)
+{
+    float usgRate=0.0;
+    int usgRateLen =0;
+    usgRate = CpuUsAge->cpu[0].cpu_usageRate/100.0;
+    for(int count =0;count<LineChartLength-1;count++)
+    {
+        cpuAllUsageLineChartDat[count] = cpuAllUsageLineChartDat[count+1];
+    }
+    cpuAllUsageLineChartDat[LineChartLength-1] = usgRate*LineChartWidth;
 }
 
 void Draw_fillInfo(int temp,cpu_all* CpuUsAge)
@@ -59,9 +76,15 @@ void Draw_fillInfo(int temp,cpu_all* CpuUsAge)
     OledPaint.Show.Number(42,0,temp,2,16);
     for(int countCpuNum=0;countCpuNum<4;countCpuNum++)
     {
-        usgRate = CpuUsAge->cpu[countCpuNum].cpu_usageRate/100.0;
+        usgRate = CpuUsAge->cpu[countCpuNum+1].cpu_usageRate/100.0;
         usgRateLen = usgRate * UsageRectLength;
         OledPaint.Draw.Rect(UsageRectBeigin_x,UsageRectBeigin_y+(UsageRectWidth+UsageRectInter)*countCpuNum,UsageRectBeigin_x+ usgRateLen,UsageRectWidth+UsageRectBeigin_y+(UsageRectWidth+UsageRectInter)*countCpuNum,1);
+    }
+    updateLineCHartDat(&CpuUsage);
+    for(int countPointNumb =0;countPointNumb<LineChartLength-1;countPointNumb++)
+    {
+        // OledPaint.Draw.Point(countPointNumb+LineChartBegin_x,LineChartBegin_y+LineChartWidth-cpuAllUsageLineChartDat[countPointNumb]);
+        OledPaint.Draw.Line(countPointNumb+LineChartBegin_x,LineChartBegin_y+LineChartWidth-cpuAllUsageLineChartDat[countPointNumb],countPointNumb+LineChartBegin_x+1,LineChartBegin_y+LineChartWidth-cpuAllUsageLineChartDat[countPointNumb+1]);
     }
     
 }
@@ -79,10 +102,12 @@ int main()
         printf("wiring init succeed\n");
     }
     Oled_DrawApi_Init();
-    CpuUsage.cpu[0] = cpu_1;
-    CpuUsage.cpu[1] = cpu_2;
-    CpuUsage.cpu[2] = cpu_3;
-    CpuUsage.cpu[3] = cpu_4;
+    CpuUsage.cpu[1] = cpu_1;
+    CpuUsage.cpu[2] = cpu_2;
+    CpuUsage.cpu[3] = cpu_3;
+    CpuUsage.cpu[4] = cpu_4;
+    CpuUsage.cpu[0] = cpu_Al;
+    memset(&cpuAllUsageLineChartDat,0,sizeof(cpuAllUsageLineChartDat));
     while(1)
     {
         
@@ -128,10 +153,10 @@ void GetInfoFromFile(int Ifpri)
     else
     {
         printf("Open Cpu info file succ\n");
-        fgets(buffer,sizeof(buffer),fp); //move fileptr to second line 
+        // fgets(buffer,sizeof(buffer),fp); //move fileptr to second line 
         if(Ifpri)
         {
-            for(int countCpuNum=0;countCpuNum<4;countCpuNum++)
+            for(int countCpuNum=0;countCpuNum<5;countCpuNum++)
             {
                 fscanf(fp,"%s %d %d %d %d %d %d %d",&CpuUsage.cpu[countCpuNum].cpuname,&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[0],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[1],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[2],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[3],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[4],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[5],&CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[6]);
                 printf("%d %d %d %d %d %d %d\n",CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[0],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[1],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[2],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[3],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[4],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[5],CpuUsage.cpu[countCpuNum].cpu_pri_usagetime[6]);
@@ -141,7 +166,7 @@ void GetInfoFromFile(int Ifpri)
         }
         else
         {
-            for(int countCpuNum=0;countCpuNum<4;countCpuNum++)
+            for(int countCpuNum=0;countCpuNum<5;countCpuNum++)
             {
                 fscanf(fp,"%s %d %d %d %d %d %d %d",&CpuUsage.cpu[countCpuNum].cpuname,&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[0],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[1],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[2],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[3],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[4],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[5],&CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[6]);
                 printf("%d %d %d %d %d %d %d\n",CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[0],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[1],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[2],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[3],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[4],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[5],CpuUsage.cpu[countCpuNum].cpu_atf_usagetime[6]);
@@ -162,10 +187,11 @@ void GetCpuUasge()
     memset(&cpu_2,0,sizeof(cpu_2));
     memset(&cpu_3,0,sizeof(cpu_3));
     memset(&cpu_4,0,sizeof(cpu_4));
+    memset(&cpu_Al,0,sizeof(cpu_Al));
     GetInfoFromFile(PriTest);
     delay(100);
     GetInfoFromFile(AftTest);
-    for(countCpuNum=0;countCpuNum<4;countCpuNum++)
+    for(countCpuNum=0;countCpuNum<5;countCpuNum++)
     {
         for(count=0;count<=6;count++)
         {
@@ -181,6 +207,6 @@ void GetCpuUasge()
     }
 
 
-    printf("cpu1:%d cpu2:%d cpu3:%d cpu4:%d \n",CpuUsage.cpu[0].cpu_usageRate,CpuUsage.cpu[1].cpu_usageRate,CpuUsage.cpu[2].cpu_usageRate,CpuUsage.cpu[3].cpu_usageRate);
+    printf("cpu:%d cpu1:%d cpu2:%d cpu3:%d cpu4:%d \n",CpuUsage.cpu[0].cpu_usageRate,CpuUsage.cpu[1].cpu_usageRate,CpuUsage.cpu[2].cpu_usageRate,CpuUsage.cpu[3].cpu_usageRate,CpuUsage.cpu[4].cpu_usageRate);
 
 }
